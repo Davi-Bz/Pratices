22902d5bc6f671d5157f34a42743814e9e0df872
Davi-Bz <102439601+Davi-Bz@users.noreply.github.com>
2022-11-23 15:23:01 -0300

    Update bin_to_dec.cpp
    new implementation

--- simple algorithm/bin_to_dec.cpp
+++ simple algorithm/bin_to_dec.cpp
@@ -2,55 +2,31 @@
 #include <vector>
 using namespace std;
 
-void fill (vector<int>& oct) {
-
-    for(int i = 0; i < oct.size(); i++){
-        std::cin >> oct[i];
-    }
-}
-
-string show (vector<int> oct) {
-    string bin;
-
-    for(int i = 0; i < oct.size(); i++){
-        bin += to_string(oct[i]);
-    }
-    return bin;
-
-}
 
 /*
     - Receber um octeto de zeros e uns;
     - Transformar esse octeto binário para um número decimal;
 */
 
-int conversion (vector<int> oct) {
+int conversion (string octeto) {
+    vector<int> vet {128, 64, 32, 16, 8, 4, 2, 1};
+    int dec { };
 
-vector<int> vet {128, 64, 32, 16, 8, 4, 2, 1};
+    for(int i = 0; i < 8; i++){
+        if(octeto[i] == '1')
+        dec += vet[i];
+    }
 
-int dec { };
-
-for(int i = 0; i < 8; i++) {
-
-    if(oct[i] == 1)
-    dec += vet[i];
-
-}
     return dec;
-}
-
-void all(vector<int> oct) {
-
-std::cout << "Insira um octeto(8 bits): \n";
-fill(oct);
-
-std::cout << "Valor em binario: " << show(oct)       << endl;
-std::cout << "Valor em decimal: " << conversion(oct) << endl;
 }
 
 int main() {
 
-vector<int> oct (8);
-all(oct);
+    string octeto;
+    cout << "insira um octeto: \n"; 
+    cin >> octeto;
+
+    cout << "Numero em binario: " << octeto              << endl;
+    cout << "Numero em decimal: " << conversion (octeto) << endl;
 
 }
